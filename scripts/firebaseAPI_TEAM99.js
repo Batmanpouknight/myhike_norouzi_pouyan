bcfe8065ad1dae7c5030686befb3e6e8795fef34
Pouyan Norouzi <irfn7pouyan@gmail.com>
2024-02-20 10:34:02 -0800

    Did the demo

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyARJkSxlTHy9LufYzMhuBBD8HnfhaOak9A",
+    authDomain: "demo07-75d8d.firebaseapp.com",
+    projectId: "demo07-75d8d",
+    storageBucket: "demo07-75d8d.appspot.com",
+    messagingSenderId: "927596668306",
+    appId: "1:927596668306:web:635e4d3f2c65038fc0cd4b",
+    measurementId: "G-Y986JHTEBQ"
 };
 
 //--------------------------------------------
